3cf24c90399e5833e53b25c7ab922efb59927fb7
olive004 <olivia.gallup@gmail.com>
2021-12-24 14:03:42 +0100

    setting up config files

--- src/utils/system_definition/specific_systems/RNA_system.py
+++ src/utils/system_definition/specific_systems/RNA_system.py
@@ -2,7 +2,11 @@ from src.utils.system_definition.agnostic_system.base_system import BaseSystem
 
 
 class RNASystem(BaseSystem):
-    def __init__(self, config_args=None):
+    def __init__(self, config_args=None, simulator="intaRNA"):
         super().__init__()
+
+        if simulator == "intaRNA":
+            from src.utils.parameter_prediction.IntaRNA.bin.CopomuS import CopomuS
+            self.simulator = CopomuS(config_args)
 
     
