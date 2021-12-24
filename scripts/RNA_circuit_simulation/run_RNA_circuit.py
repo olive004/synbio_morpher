bac6a3f73c31e6f9e64d02d74dee629504380ae7
olive004 <olivia.gallup@gmail.com>
2021-12-24 12:20:21 +0100

    updated visualiser to include edge weights

--- scripts/RNA_circuit_simulation/run_RNA_circuit.py
+++ scripts/RNA_circuit_simulation/run_RNA_circuit.py
@@ -1,13 +1,15 @@
 from fire import Fire
 
 from src.utils.decorators import time_it
-from src.utils.system_definition.agnostic_system.base_system import BaseSystem
+from src.utils.system_definition.specific_systems import RNASystem
 
 
 @time_it
 def main(config_args=None):
-    circuit = BaseSystem(config_args)
+
+    circuit = RNASystem(config_args)
     circuit.visualise()
+
 
 if __name__ == "__main__":
     Fire(main)
--- src/utils/visualisation/graph_drawer.py
+++ src/utils/visualisation/graph_drawer.py
@@ -7,14 +7,18 @@ def visualise_graph_pyvis(graph: nx.DiGraph,
     import webbrowser
     import os
 
-    interactive_graph = Network(height=800, width=800, notebook=True)
-    interactive_graph.from_nx(graph)
+    interactive_graph = Network(height=800, width=800, directed=True, notebook=True)
+    interactive_graph.from_nx(graph, edge_weight_transf=lambda x: round(x, 4))
+    # interactive_graph.show_buttons(filter_=['edges'])
+    interactive_graph.inherit_edge_colors(True)
+    interactive_graph.set_edge_smooth('dynamic')
     interactive_graph.show(plot_name)
 
     print("Opening graph in browser...")
     web_filename = 'file:///' + os.getcwd() + '/' + plot_name
     webbrowser.open(web_filename, new=1, autoraise=True)
 
+    print('debug over')
 
 def visualise_graph_pyplot(graph: nx.DiGraph):
     import matplotlib.pyplot as plt
