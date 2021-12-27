cad26ad70d8a32f1613e00b9041e02716d1a2426
olive004 <olivia.gallup@gmail.com>
2021-12-27 15:51:10 +0100

    install argparse

--- src/utils/system_definition/configurations.py
+++ src/utils/system_definition/configurations.py
@@ -1,22 +1,29 @@
 import json
+import argparse
 from typing import Dict
 
 
-def parse_kwargs(config_file: str = None) -> Dict:
+def parse_args(config_file: str = None, all_args: Dict = None) -> Dict:
 
-    all_args = retrieve_default_args()
+    if all_args is None:
+        all_args = retrieve_default_args()
 
     if config_file is not None:
-        with open(config_file) as json_file:
-            configs = json.load(json_file)
+        configs = json.load(open(config_file))
 
         all_args = all_args | configs
+    parser = argparse.ArgumentParser()
+    update_namespace_with_dict(parser.parse_args(), all_args)
     return all_args
 
 
 def retrieve_default_args() -> Dict:
-    
+
     fn = "scripts/common/default_args.json"
-    with open(fn) as json_file:
-        default_args = json.load(json_file)
+    default_args = json.load(open(fn))
     return default_args
+
+
+def update_namespace_with_dict(args: Namespace, updater_dict: Dict):
+    for k in updater_dict:
+        vars(args).update(updater_dict[k])
