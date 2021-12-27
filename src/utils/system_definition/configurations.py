5aacfbf919914dadc1ea8b3455fcea4c7e2016c3
olive004 <olivia.gallup@gmail.com>
2021-12-27 16:41:44 +0100

    adding simulator getter & simulator cfg parser

--- src/utils/system_definition/configurations.py
+++ src/utils/system_definition/configurations.py
@@ -1,6 +1,15 @@
-import json
 import argparse
+import json
+import logging
+import os
 from typing import Dict
+
+
+def get_simulator_names():
+    logging.debug(os.getcwd())
+    simulator_dir = os.join(os.getcwd(), "src", "utils", "parameter_prediction")
+    simulators = {f for f in os.walk(simulator_dir)}
+    return simulators
 
 
 def parse_args(config_file: str = None, all_args: Dict = None) -> Dict:
@@ -13,17 +22,30 @@ def parse_args(config_file: str = None, all_args: Dict = None) -> Dict:
 
         all_args = all_args | configs
     parser = argparse.ArgumentParser()
-    update_namespace_with_dict(parser.parse_args(), all_args)
-    return all_args
+    args_namespace = parser.parse_args()
+    update_namespace_with_dict(args_namespace, all_args)
+    return args_namespace
+
+
+def parse_simulation_args(dict_args):
+    simulators = get_simulator_names()
+    config_path = {}
+    for simulator_name in simulators:
+        if simulator_name in dict_args:
+            config_path[simulator_name] = dict_args[simulator_name]
+            try:
+                jf = json.load(config_path[simulator_name])
+            except FileNotFoundError:
+                logging.error(f'Path to simulator {simulator_name} not found')
+    return config_path
 
 
 def retrieve_default_args() -> Dict:
-
     fn = "scripts/common/default_args.json"
     default_args = json.load(open(fn))
     return default_args
 
 
-def update_namespace_with_dict(args: Namespace, updater_dict: Dict):
-    for k in updater_dict:
-        vars(args).update(updater_dict[k])
+def update_namespace_with_dict(args, updater_dict: Dict):
+    vars(args).update(updater_dict)
+    return args
