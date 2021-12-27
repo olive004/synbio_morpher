4d51b481de1835437459da4f8e508d06552e2354
olive004 <olivia.gallup@gmail.com>
2021-12-27 16:41:56 +0100

    adding simulator getter & simulator cfg parser

--- scripts/RNA_circuit_simulation/run_RNA_circuit.py
+++ scripts/RNA_circuit_simulation/run_RNA_circuit.py
@@ -1,14 +1,14 @@
 from fire import Fire
 
 from src.utils.decorators import time_it
-from src.utils.system_definition.configurations import parse_kwargs
+from src.utils.system_definition.configurations import parse_args
 from src.utils.system_definition.specific_systems.RNA_system import RNASystem
 
 
 @time_it
 def main(config_file=None):
 
-    parsed_args = parse_kwargs(config_file)
+    parsed_args = parse_args(config_file)
 
     circuit = RNASystem(parsed_args)
     circuit.visualise()
