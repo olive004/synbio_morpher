c30ff1a9c04107d2a5f52b4e570914eef48547a7
olive004 <olivia.gallup@gmail.com>
2022-01-03 11:51:16 +0100

    auto install bash script for ViennaRNA and IntaRNA

--- run_all.sh
+++ run_all.sh
@@ -5,15 +5,7 @@ CURRENT_ENV_PATH = /Users/oliviagallup/Desktop/Kode/Oxford/DPhil/env_gene_circ/b
 source $CURRENT_ENV_PATH
 
 # Configure non-pip dependencies
-cd .src/utils/parameter_prediction/IntaRNA-3.2.1
-./configure
-make
-make tests
-make install prefix=$CURRENT_ENV_PATH
-cd ../../..
+source ./src/setup/install_deps.sh
 
 # Clean up dependencies
-cd .src/utils/parameter_prediction/IntaRNA-3.2.1
-make clean
-make uninstall
-cd ../../..
+source ./src/setup/remove_deps.sh
