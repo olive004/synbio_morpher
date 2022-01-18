08a6544b070e39bf679b438e2e841382fbb039fb
olive004 <olivia.gallup@gmail.com>
2022-01-18 21:33:32 +0000

    IntaRNA works wow

--- src/utils/parameter_prediction/simulator_loading.py
+++ src/utils/parameter_prediction/simulator_loading.py
@@ -15,11 +15,5 @@ def load_IntaRNA(args: dict) -> dict:
     args['query'] = load_seq_from_FASTA(args['query'])[0]
     args['target'] = load_seq_from_FASTA(args['target'])[0]
 
-    # path_IntaRNA = "/Users/oliviagallup/Desktop/Kode/Oxford/DPhil/Gene_circuit_glitch_prediction/src/utils/parameter_prediction/IntaRNA"
-    # path_IntaRNA = "/Users/oliviagallup/Desktop/Kode/Oxford/DPhil/Gene_circuit_glitch_prediction/src/utils/parameter_prediction/IntaRNA/lib"
-    # path_IntaRNA = "/Users/oliviagallup/Desktop/Kode/Oxford/DPhil/Gene_circuit_glitch_prediction/src/utils/parameter_prediction/IntaRNA/lib/libIntaRNA.a"
-    # path_IntaRNA = "/Users/oliviagallup/Desktop/Kode/Oxford/DPhil/Gene_circuit_glitch_prediction/src/utils/parameter_prediction/IntaRNA/lib/pkgconfig/IntaRNA.pc"
-    # path_IntaRNA = "/Users/oliviagallup/Desktop/Kode/Oxford/DPhil/Gene_circuit_glitch_prediction/src/utils/parameter_prediction/IntaRNA/bin/copomus/IntaRNA.py"
-    # path_IntaRNA = "/Users/oliviagallup/Desktop/Kode/Oxford/DPhil/Gene_circuit_glitch_prediction/src/utils/parameter_prediction/IntaRNA/include/IntaRNA"
     # sys.path.insert(0, path_IntaRNA)
     return args
