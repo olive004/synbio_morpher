79d57fa80a264da4d4b986d1427ae8c07615f395
olive004 <olivia.gallup@gmail.com>
2022-01-23 12:45:24 +0000

    need to resolve circular properties

--- src/utils/system_definition/agnostic_system/base_system.py
+++ src/utils/system_definition/agnostic_system/base_system.py
@@ -1,84 +1,10 @@
+from hashlib import new
+from multiprocessing.sharedctypes import Value
 import numpy as np
 import networkx as nx
+import logging
 
 from src.utils.data.fake_data_generation.toy_graphs import square_matrix_rand
-
-
-class BaseSystem():
-    def __init__(self, config_args: dict = None):
-
-        if config_args is None:
-            config_args = {}
-
-        self.data = config_args.get("data", None)
-        self.interactions = self.init_interaction_matrix()
-
-    def build_graph(self, source_matrix=None) -> nx.DiGraph:
-        if source_matrix is not None:
-            inters = source_matrix
-        else: 
-            inters = self.interactions
-        graph = nx.from_numpy_matrix(inters, create_using=nx.DiGraph)
-        graph = nx.relabel_nodes(graph, self._node_labels)
-        return graph
-
-    def refresh_graph(self):
-        self.graph = self.build_graph()
-    
-    def get_graph_labels(self) -> dict:
-        return sorted(self.graph)
-
-    def determine_input_type(self) -> str:
-        raise NotImplementedError
-
-    def init_interaction_matrix(self) -> np.array:
-        matrix_size = self.data.size
-        return InteractionMatrix(num_nodes=matrix_size).matrix
-
-    def visualise(self, mode="pyvis"):
-        self.graph = self.build_graph(self.interactions)
-
-        if self.node_labels:
-            self.graph = nx.relabel_nodes(self.graph, self.node_labels)
-
-        if mode == 'pyvis':
-            from src.utils.visualisation.graph_drawer import visualise_graph_pyvis
-            visualise_graph_pyvis(self.graph)
-        else:
-            from src.utils.visualisation.graph_drawer import visualise_graph_pyplot
-            visualise_graph_pyplot(self.graph)
-
-    @property
-    def graph(self):
-        self._graph = self.build_graph(self.interactions)
-        return self._graph
-
-    @graph.setter
-    def graph(self, new_graph):
-        assert type(new_graph) == nx.DiGraph, 'Cannot set graph to' + \
-            f' type {type(new_graph)}.'
-        self._graph = new_graph
-
-    @property
-    def node_labels(self):
-        self._node_labels = None
-        return self._node_labels
-
-    # @node_labels.getter
-    # def node_labels(self):
-    #     return self._node_labels
-
-    @node_labels.setter
-    def node_labels(self, labels: dict):
-        if type(labels) == list:
-            current_nodes = self.get_graph_labels()
-            self._node_labels = dict(zip(current_nodes, labels))
-        elif type(labels) == dict:
-            self._node_labels = labels
-        self.refresh_graph()
-
-        self.graph = nx.relabel_nodes(self.graph, self._node_labels)
-
 
 
 class BaseSpecies():
@@ -120,3 +46,93 @@ class InteractionMatrix():
             max_nodes = 15
             num_nodes = np.random.randint(min_nodes, max_nodes)
         return self.make_rand_matrix(num_nodes)
+
+
+class BaseSystem():
+    def __init__(self, config_args: dict = None):
+
+        if config_args is None:
+            config_args = {}
+
+        self.data = config_args.get("data", None)
+        self.interactions = self.init_interaction_matrix()
+        self.node_labels = None
+
+    def build_graph(self, source_matrix=None) -> nx.DiGraph:
+        if source_matrix is not None:
+            inters = source_matrix
+        else: 
+            inters = self.interactions
+        logging.debug('interactions')
+        logging.debug(self.interactions)
+        graph = nx.from_numpy_matrix(inters, create_using=nx.DiGraph)
+        if self.node_labels is not None:
+            graph = nx.relabel_nodes(graph, self.node_labels)
+        return graph
+
+    def refresh_graph(self):
+        self.graph = self.build_graph()
+    
+    def get_graph_labels(self) -> dict:
+        return sorted(self.graph)
+
+    def determine_input_type(self) -> str:
+        raise NotImplementedError
+
+    def init_interaction_matrix(self) -> np.array:
+        matrix_size = np.random.randint(5) if self.data is None \
+            else self.data.size
+        return InteractionMatrix(num_nodes=matrix_size).matrix
+
+    def visualise(self, mode="pyvis"):
+        self.refresh_graph()
+        logging.debug('graph labels')
+        logging.debug(self.get_graph_labels())
+
+        if mode == 'pyvis':
+            from src.utils.visualisation.graph_drawer import visualise_graph_pyvis
+            visualise_graph_pyvis(self.graph)
+        else:
+            from src.utils.visualisation.graph_drawer import visualise_graph_pyplot
+            visualise_graph_pyplot(self.graph)
+
+    @property
+    def graph(self):
+        return self._graph
+
+    @graph.setter
+    def graph(self, new_graph):
+        assert type(new_graph) == nx.DiGraph, 'Cannot set graph to' + \
+            f' type {type(new_graph)}.'
+        self._graph = new_graph
+
+    @property
+    def interactions(self):
+        return self._interactions
+
+    @interactions.setter
+    def interactions(self, new_interactions):
+        if type(new_interactions) == np.ndarray:
+            self._interactions = new_interactions
+        else:
+            raise ValueError('Cannot set interactions to' + \
+            f' type {type(new_interactions)}.')
+
+    @property
+    def node_labels(self):
+        print('node label get')
+        return self._node_labels
+
+    @node_labels.setter
+    def node_labels(self, labels: dict):
+        logging.debug('node_labels set')
+        current_nodes = self.get_graph_labels()
+        if type(labels) == list:
+            self._node_labels = dict(zip(current_nodes, labels))
+        else:
+            labels = list(range(len(self.interactions)))
+            self._node_labels = dict(zip(current_nodes, labels))
+        self.graph = nx.relabel_nodes(self.graph, self._node_labels)
+        logging.debug('nl graph labels')
+        logging.debug(self.get_graph_labels())
+
--- src/utils/system_definition/specific_systems/RNA_system.py
+++ src/utils/system_definition/specific_systems/RNA_system.py
@@ -16,19 +16,27 @@ class RNASystem(BaseSystem):
         self.simulator = InteractionSimulator(
             self.simulator_args, self.simulator_choice)
 
-        self.run_simulator()
+        self.simulate_interaction_strengths()
         
     def get_part_to_part_intrs(self):
         data = self.run_simulator()
+        print(data)
 
     def run_simulator(self):
         return self.simulator.run()
 
     def process_data(self):
         self.nodes_labels = self.data.sample_names
+        self.nodes_labels = 1
+
+        logging.debug('pd graph labels')
+        logging.debug(self.get_graph_labels())
+        logging.debug('self.nodes_labels')
+        logging.debug(self.nodes_labels)
 
     def simulate_interaction_strengths(self):
-        self.interactions = self.get_part_to_part_intrs()
+        # self.interactions = self.get_part_to_part_intrs()
+        pass
 
 
 class RNASpecies(BaseSpecies):
