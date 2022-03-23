3b3c925ca1ea8b5cced515a9f921fc01e09fcb4a
olive004 <olivia.gallup@gmail.com>
2022-03-23 15:16:55 +0000

    adaptation signal from Nature article

--- src/utils/signal/inputs.py
+++ src/utils/signal/inputs.py
@@ -3,13 +3,16 @@ import numpy as np
 
 
 class Signal():
-    def __init__(self, in_magnitude, total_time, idx_identity, signal=None) -> None:
-        self.total_time = total_time
-        self.idx_identity = idx_identity
-        self.abstract_signal = signal if signal is not None else [0, 1, 0, 1]
-        self.magnitude = in_magnitude
-        time_steps = total_time / len(self.abstract_signal)
-        self.time_dilation_func = partial(np.repeat, repeats=time_steps)
+    def __init__(self, identities_idx, signal=None,
+                 total_time=None,
+                 magnitude=1) -> None:
+        self.identities_idx = identities_idx
+        self.abstract_signal = signal if signal is not None else [0, 1]
+        self.total_time = total_time if total_time else len(
+            self.abstract_signal)
+        self.magnitude = magnitude
+        self.time_steps = total_time / len(self.abstract_signal)
+        self.time_dilation_func = partial(np.repeat, repeats=self.time_steps)
 
     @property
     def abstract_signal(self):
@@ -25,3 +28,28 @@ class Signal():
 
     def follow(self):
         pass
+
+
+class AdaptationTarget(Signal):
+    def __init__(self, identities_idx, signal=None, total_time=None, magnitude=1) -> None:
+        super().__init__(identities_idx, signal, total_time, magnitude)
+
+    @property
+    def real_signal(self):
+        return self.adapt_pulse_double_exp(len(self.time_steps), height=self.magnitude)
+
+    def adapt_pulse_double_exp(time_points: int, height) -> np.array:
+        ''' Double exponential response curve
+        From Shen et al. 2021 https://doi.org/10.1038/s41467-021-23420-5 
+        https://github.com/sjx93/rnn_for_gene_network_2020/blob/main/fig1-2_adaptation/target_response_curves.py '''
+        # inputs: scalars
+        # output: np.array of shape=time_points
+        # double exponential response curve
+        xs0 = np.linspace(0.0, (time_points-1), time_points)
+        y = height*2*(np.exp(-xs0/6) - np.exp(-xs0/3))
+        return y
+
+
+class OscillatingSignal(Signal):
+    def __init__(self, identities_idx, signal=None, total_time=None, magnitude=1) -> None:
+        super().__init__(identities_idx, signal, total_time, magnitude)
