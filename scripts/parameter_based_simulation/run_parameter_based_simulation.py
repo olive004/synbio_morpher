e8ee4426e40f789283666264bd822dda3943801a
olive004 <olivia.gallup@gmail.com>
2022-06-02 00:27:57 +0100

    changing iterator logic

--- scripts/parameter_based_simulation/run_parameter_based_simulation.py
+++ scripts/parameter_based_simulation/run_parameter_based_simulation.py
@@ -49,12 +49,16 @@ def main(config=None, data_writer=None):
     logging.info("all_species_steady_states")
     logging.info(np.shape(all_species_steady_states))
     num_iterations = matrix_size
-    for i in range(num_iterations):
+    for i in range(230): #num_iterations):
         logging.info(f'Iteration {i}/{num_iterations}')
         flat_triangle = np.zeros(num_unique_interactions)
-        for j in range(num_unique_interactions):
-            v = np.mod(i, np.power(size_interaction_array, j))
-            flat_triangle[j] = interaction_array[v]
+        # logging.info('v')
+        # logging.info(np.mod(i, np.power(size_interaction_array, np.arange(1, num_unique_interactions+1))))
+        vs = []
+        iterators = [[int(np.mod(i / np.power(size_interaction_array, j), size_interaction_array))] for j in range(num_unique_interactions)]
+            # idxs = [np.tile(np.repeat(tile_pattern, 3), int(3*np.power(3, 6)/len(np.repeat(tile_pattern, 3))))]
+            # v = np.mod(i, np.power(size_interaction_array, j+1))
+        flat_triangle[:] = interaction_array[tuple(iterators)]
         interaction_matrix = make_symmetrical_matrix_from_sequence(
             flat_triangle, num_species)
         cfg = {"interactions": {
@@ -63,16 +67,18 @@ def main(config=None, data_writer=None):
             "sample_names": sample_names
         }
         logging.info("interaction_matrix")
-        logging.info(interaction_matrix)
+        logging.info('\n' + str(interaction_matrix))
 
         circuit = construct_circuit_from_cfg(
             extra_configs=cfg, config_filepath=config)
         circuit = CircuitModeller(result_writer=data_writer).find_steady_states(circuit)
-        idx = [np.mod(i, np.power(size_interaction_array, j)) for j in range(num_unique_interactions)]
+        idx = [slice(0, num_species)] + iterators
         logging.info("idx")
         logging.info(idx)
         logging.info("circuit.species.steady_state_copynums")
         logging.info(circuit.species.steady_state_copynums)
-        all_species_steady_states[:, 0,0,0,0,0,0] = flatten_listlike(circuit.species.steady_state_copynums)
-        logging.info(all_species_steady_states)
-        break
+        # all_species_steady_states[slice(0, num_species), 0,0,0,0,0,0] = flatten_listlike(circuit.species.steady_state_copynums)
+        all_species_steady_states[tuple(idx)] = circuit.species.steady_state_copynums[:]
+        logging.info(all_species_steady_states[tuple(idx)])
+    logging.info(all_species_steady_states[:, 0,3,0,0,0,0])
+    logging.info(all_species_steady_states[:, 11,4,0,0,0,0])
