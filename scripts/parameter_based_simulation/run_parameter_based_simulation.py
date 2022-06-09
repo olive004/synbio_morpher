a588125d96ffd52a4a37c10697b4ab76620e805a
olive004 <olivia.gallup@gmail.com>
2022-06-09 14:55:21 +0100

    consolidating anlytics variables in script

--- scripts/parameter_based_simulation/run_parameter_based_simulation.py
+++ scripts/parameter_based_simulation/run_parameter_based_simulation.py
@@ -60,25 +60,9 @@ def main_subprocess(config, data_writer, sub_process, total_processes):
         assert matrix_size == np.prod(list(
             matrix_dimensions)), 'Something is off about the intended size of the matrix'
 
-        all_species_steady_states = np.zeros(
-            matrix_dimensions, dtype=np.float32)
-        all_species_response_time = np.zeros(
-            matrix_dimensions, dtype=np.float32)
-        all_species_response_time_low = np.zeros(
-            matrix_dimensions, dtype=np.float32)
-        all_species_response_time_high = np.zeros(
-            matrix_dimensions, dtype=np.float32)
-        all_species_overshoot = np.zeros(
-            matrix_dimensions, dtype=np.float32)
-        all_species_fold_change = np.zeros(
-            matrix_dimensions, dtype=np.float32)
-        all_species_precision = np.zeros(
-            matrix_dimensions, dtype=np.float32)
-        all_species_sensitivity = np.zeros(
-            matrix_dimensions, dtype=np.float32)
-
         all_species_analytics = []
-        for analytic in Timeseries(None).get_analytics_types():
+        analytic_types = Timeseries(None).get_analytics_types()
+        for analytic in analytic_types:
             all_species_analytics.append(np.zeros(
                 matrix_dimensions, dtype=np.float32))
 
@@ -100,6 +84,71 @@ def main_subprocess(config, data_writer, sub_process, total_processes):
             f'\t{np.round(100.7/500*num_iterations/1000, decimals=3)}Gb')
         modeller = CircuitModeller(result_writer=data_writer)
 
+        # @time_it
+        def loop_iter():
+            """ Timings:
+            'naive' signal simulation
+            0.0010s 
+            Function name: construct_circuit_from_cfg
+
+            0.0224s 
+            Function name: init_circuit
+
+            0.2604s 
+            Function name: simulate_signal
+
+            0.0393s 
+            Function name: write_all
+
+
+            'ivp' signal simulation
+            0.1569s 
+            Function name: loop_iter
+
+            0.0008s 
+            Function name: construct_circuit_from_cfg
+
+            0.0183s 
+            Function name: init_circuit
+
+            0.1026s 
+            Function name: simulate_signal
+
+            0.0396s 
+            Function name: write_all
+
+            """
+
+            iterators = [int(np.mod(i / np.power(size_interaction_array, j),
+                                    size_interaction_array)) for j in range(num_unique_interactions)]
+            flat_triangle = interaction_array[list(iterators)]
+            interaction_matrix = make_symmetrical_matrix_from_sequence(
+                flat_triangle, num_species)
+            cfg = {"interactions": {
+                "interactions_matrix": interaction_matrix,
+                "interactions_units": SIMULATOR_UNITS['IntaRNA']['rate']}
+            }
+
+            circuit = construct_circuit_from_cfg(
+                extra_configs=cfg, config_filepath=config)
+            circuit = modeller.init_circuit(circuit)
+            circuit = modeller.simulate_signal(
+                circuit, use_solver=config_file.get('signal').get('use_solver', 'naive'))
+
+            idxs = [slice(0, num_species)] + [[ite] for ite in iterators]
+            for i, analytic in enumerate(analytic_types):
+                all_species_analytics[i][tuple(idxs)] = circuit.result_collector.results['signal'].analytics.get(analytic)
+
+            # @time_it
+            if np.mod(i, 100) == 0:
+                write_all()
+
+        def write_all(out_type='npy'):
+            for i, analytic in enumerate(analytic_types):
+                data_writer.output(out_type, out_name=f'all_species_{analytic}',
+                                    data=all_species_analytics[i].astype(np.float32), overwrite=True,
+                                    write_to_top_dir=True)
+
         for i in range(starting_iteration, end_iteration):  # 8100 per min
             if np.mod(i, 1000) == 0 or i == starting_iteration:
                 data_writer.unsubdivide()
@@ -109,135 +158,11 @@ def main_subprocess(config, data_writer, sub_process, total_processes):
                     f'Iteration {i}/{total_iterations}, stopping at {end_iteration}')
             data_writer.subdivide_writing(str(i), safe_dir_change=False)
 
-            # @time_it
-            def loop_iter():
-                """ Timings:
-                'naive' signal simulation
-                0.0010s 
-                Function name: construct_circuit_from_cfg
-
-                0.0224s 
-                Function name: init_circuit
-
-                0.2604s 
-                Function name: simulate_signal
-
-                0.0393s 
-                Function name: write_all
-
-
-                'ivp' signal simulation
-                0.1569s 
-                Function name: loop_iter
-
-                0.0008s 
-                Function name: construct_circuit_from_cfg
-
-                0.0183s 
-                Function name: init_circuit
-
-                0.1026s 
-                Function name: simulate_signal
-
-                0.0396s 
-                Function name: write_all
-
-                """
-
-                iterators = [int(np.mod(i / np.power(size_interaction_array, j),
-                                        size_interaction_array)) for j in range(num_unique_interactions)]
-                flat_triangle = interaction_array[list(iterators)]
-                interaction_matrix = make_symmetrical_matrix_from_sequence(
-                    flat_triangle, num_species)
-                cfg = {"interactions": {
-                    "interactions_matrix": interaction_matrix,
-                    "interactions_units": SIMULATOR_UNITS['IntaRNA']['rate']}
-                }
-
-                circuit = construct_circuit_from_cfg(
-                    extra_configs=cfg, config_filepath=config)
-                circuit = modeller.init_circuit(circuit)
-                circuit = modeller.simulate_signal(
-                    circuit, use_solver=config_file.get('signal').get('use_solver', 'naive'))
-
-                idxs = [slice(0, num_species)] + [[ite] for ite in iterators]
-                all_species_steady_states[tuple(
-                    idxs)] = circuit.species.steady_state_copynums[:].astype(np.float32)
-                all_species_response_time[tuple(
-                    idxs)] = circuit.result_collector.results['signal'].analytics.get('response_time')
-                all_species_response_time_low[tuple(
-                    idxs)] = circuit.result_collector.results['signal'].analytics.get('response_time_low')
-                all_species_response_time_high[tuple(
-                    idxs)] = circuit.result_collector.results['signal'].analytics['response_time_high']
-                all_species_overshoot[tuple(
-                    idxs)] = circuit.result_collector.results['signal'].analytics['overshoot']
-                all_species_fold_change[tuple(
-                    idxs)] = circuit.result_collector.results['signal'].analytics['fold_change']
-                all_species_precision[tuple(
-                    idxs)] = circuit.result_collector.results['signal'].analytics['precision']
-                all_species_sensitivity[tuple(
-                    idxs)] = circuit.result_collector.results['signal'].analytics['sensitivity']
-
-                # logging.info(circuit.result_collector.results['signal'].analytics)
-
-                # logging.info(circuit.result_collector.results['signal'].analytics.get('response_time'))
-                # logging.info(all_species_response_time[all_species_response_time>0])
-
-                # @time_it
-
-                def write_all():
-                    # data_writer.output(
-                    #     'csv', out_name='flat_triangle_interaction_matrix', data=flat_triangle)
-                    # data_writer.output('csv', out_name='steady_state',
-                    #                 data=circuit.species.steady_state_copynums[:])
-                    # modeller.write_results(circuit=circuit, no_visualisations=False, only_numerical=False)
-
-                    for out_type in ['npy']:
-                        data_writer.output(out_type, out_name='all_species_steady_states',
-                                           data=all_species_steady_states.astype(np.float32), overwrite=True,
-                                           write_to_top_dir=True)
-                        data_writer.output(out_type, out_name='all_species_response_time',
-                                           data=all_species_response_time.astype(np.float32), overwrite=True,
-                                           write_to_top_dir=True)
-                        data_writer.output(out_type, out_name='all_species_response_time_low',
-                                           data=all_species_response_time_low.astype(np.float32), overwrite=True,
-                                           write_to_top_dir=True)
-                        data_writer.output(out_type, out_name='all_species_response_time_high',
-                                           data=all_species_response_time_high.astype(np.float32), overwrite=True,
-                                           write_to_top_dir=True)
-                        data_writer.output(out_type, out_name='all_species_response_time_high',
-                                           data=all_species_response_time_high.astype(np.float32), overwrite=True,
-                                           write_to_top_dir=True)
-                        data_writer.output(out_type, out_name='all_species_response_time_high',
-                                           data=all_species_response_time_high.astype(np.float32), overwrite=True,
-                                           write_to_top_dir=True)
-                        data_writer.output(out_type, out_name='all_species_response_time_high',
-                                           data=all_species_response_time_high.astype(np.float32), overwrite=True,
-                                           write_to_top_dir=True)
-                        data_writer.output(out_type, out_name='all_species_response_time_high',
-                                           data=all_species_response_time_high.astype(np.float32), overwrite=True,
-                                           write_to_top_dir=True)
-                if np.mod(i, 100) == 0:
-                    write_all()
-            # experiment = Experiment(config_filepath=config, protocols=[Protocol(loop_iter)],
-            #                         data_writer=data_writer)
-            # experiment.run_experiment()
             loop_iter()
             data_writer.unsubdivide_last_dir()
 
         logging.info('Finished: outputting final matrices')
-        data_writer.output('npy', out_name='steady_state_interpolation',
-                           data=all_species_steady_states.astype(np.float32), overwrite=True,
-                           write_to_top_dir=True)
-        data_writer.output('npy', out_name='all_species_response_time',
-                           data=all_species_response_time.astype(np.float32), overwrite=True,
-                           write_to_top_dir=True)
-        data_writer.output('npy', out_name='all_species_response_time_low',
-                           data=all_species_response_time_low.astype(np.float32), overwrite=True,
-                           write_to_top_dir=True)
-        data_writer.output('npy', out_name='all_species_response_time_high',
-                           data=all_species_response_time_high.astype(np.float32), overwrite=True,
-                           write_to_top_dir=True)
+        write_all()
 
     experiment = Experiment(config_filepath=config, protocols=[Protocol(make_interaction_interpolation_matrices)],
                             data_writer=data_writer)
