1564b977c4c799085449dab32228ec4b1b2360f7
olive004 <olivia.gallup@gmail.com>
2022-07-07 14:46:59 +0100

    update run_main

--- run_main.py
+++ run_main.py
@@ -1,7 +1,7 @@
 import logging
 from fire import Fire
 # from scripts.agnostic_simulation.run_agnostic_simulation import main
-from scripts.RNA_circuit_simulation.run_RNA_circuit import main
+# from scripts.RNA_circuit_simulation.run_RNA_circuit import main
 # from src.utils.data.fake_data_generation.nc_sequences import main
 # from scripts.pair_species_mutation.run_pair_species_mutation import main
 # from scripts.generate_species_templates.run_generate_species_templates import main
@@ -10,7 +10,7 @@ from scripts.RNA_circuit_simulation.run_RNA_circuit import main
 # from scripts.analyse_mutated_templates.run_analyse_mutated_templates import main
 # from scripts.intarna_sandbox.run_intarna_sandbox import main
 # from scripts.ensemble_mutation_effect_analysis.run_ensemble_mutation_effect_analysis import main
-# from scripts.parameter_based_simulation.run_parameter_based_simulation import main
+from scripts.parameter_based_simulation.run_parameter_based_simulation import main
 # from scripts.stitch_parameter_grid.run_stitch_parameter_grid import main
 # from scripts.parameter_grid_analysis.run_parameter_grid_analysis import main
 
