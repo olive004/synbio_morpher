80ce412d26704e1633f9daa40b1d43ffba85cd31
olive004 <olivia.gallup@gmail.com>
2022-07-15 21:02:16 +0100

    trying new config format

--- scripts/parameter_grid_analysis/run_ensemble_parameter_grid_analysis.py
+++ scripts/parameter_grid_analysis/run_ensemble_parameter_grid_analysis.py
@@ -8,6 +8,50 @@ def main(config=None, data_writer=None):
         'scripts', 'parameter_grid_analysis', 'configs', 'heatmap_cfg.json'))
 
 
-    config['']
+    v = {"interacting_species": {
+            "0": ["RNA_0", "RNA_1"],
+            "1": ["RNA_0", "RNA_2"]
+        },
+        "strengths": {
+            "0": {"start_value": 0, "end_value": 1, "step_size": None},
+            "1": {"start_value": 0, "end_value": 1, "step_size": None}
+        },
+        "non_varying_species_interactions": {
+            "0": ["RNA_0", "RNA_0"],
+            "1": ["RNA_1", "RNA_1"],
+            "2": ["RNA_2", "RNA_2"],
+            "4": ["RNA_1", "RNA_2"]
+        },
+        "non_varying_strengths": {
+            "0": 0,
+            "1": 0,
+            "2": 0,
+            "4": 0
+        }
+    }
 
-    Ensembler(data_writer=data_writer, config=config, subscripts=)
+    v = {
+        ("RNA_0", "RNA_0"): {
+            "strengths": {"start_value": 0, "end_value": 1, "step_size": None}
+        },
+        ("RNA_0", "RNA_1"): {
+            "non_varying_species_interactions": 0
+        },
+        ("RNA_0", "RNA_2"): {
+            "strengths": {"start_value": 0, "end_value": 1, "step_size": None}
+        },
+        ("RNA_1", "RNA_1"): {
+            "non_varying_species_interactions": 0
+        },
+        ("RNA_1", "RNA_2"): {
+            "non_varying_species_interactions": 0
+        },
+        ("RNA_2", "RNA_1"): {
+            "non_varying_species_interactions": 0
+        }
+    }
+
+    config["slicing"]["interactions"]
+
+    for species_interaction, interaction_strength_cfg in v.items():
+        
