f77c34a987bf3b16eed8054d690e304a3fb57378
olive004 <olivia.gallup@gmail.com>
2022-09-01 17:30:30 +0100

    debug: gh a pwd

--- setup/start_docker.sh
+++ setup/start_docker.sh
@@ -1,10 +1,10 @@
 docker pull docker/dockerfile:1
 docker pull quay.io/biocontainers/intarna:3.3.1--pl5321h7ff8a90_1
 
+pwd
 # If image not built yet
 docker build -t genetic_glitch:latest docker_unix
 
-pwd
 
 docker create -it \
 --rm \
