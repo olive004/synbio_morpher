cc2b14e2169decec4e858531bb1c83570ebbd97e
olive004 <olivia.gallup@gmail.com>
2022-11-09 14:50:57 +0000

    bad: broken imports

--- src/utils/circuit/agnostic_circuits/circuit_manager_new.py
+++ src/utils/circuit/agnostic_circuits/circuit_manager_new.py
@@ -18,26 +18,9 @@ from src.utils.signal.signals import Signal
 from src.utils.circuit.agnostic_circuits.circuit_new import Circuit
 from src.utils.modelling.deterministic import Deterministic, simulate_signal_scan, bioreaction_sim_full
 from src.utils.modelling.base import Modeller
-from src.clients.common.setup import compose_kwargs, instantiate_system, construct_signal
 
 
 TEST_MODE = False
-
-
-# @time_it
-def construct_circuit_from_cfg(extra_configs: dict, config_filepath: str = None, config_file: dict = None):
-
-    kwargs = compose_kwargs(config_filepath=config_filepath,
-                            extra_configs=extra_configs, config_file=config_file)
-    circuit = instantiate_system(kwargs)
-
-    if kwargs.get("signal"):
-        kwargs.get("signal")[
-            "identities_idx"] = circuit.species.identities['input']
-        signal = construct_signal(kwargs.get("signal"))
-        circuit.signal = signal
-    return circuit
-
 
 
 class CircuitModeller():
--- src/utils/common/setup_new.py
+++ src/utils/common/setup_new.py
@@ -1,5 +1,5 @@
 import logging
-import bioreaction
+from bioreaction.model.data_tools import construct_model_fromnames
 from src.srv.io.manage.sys_interface import make_filename_safely
 from src.utils.data.data_format_tools.common import load_json_as_dict
 from src.srv.io.manage.data_manager import DataManager
@@ -21,7 +21,20 @@ ESSENTIAL_KWARGS = [
 ]
 
 
-def compose_kwargs(extra_configs: dict = None, config_filepath: str = None, config_file: dict = None) -> dict:
+def construct_bioreaction_model(sample_names, molecular_params):
+    model = construct_model_fromnames(sample_names)
+    # Add rates...
+    for i in range(len(model.reactions)):
+        if model.reactions[i].input == []:
+            model.reactions[i].forward_rates = molecular_params.get('creation_rates')
+            model.reactions[i].reverse_rates = 0
+        if model.reactions[i].output == []:
+            model.reactions[i].reverse_rates = molecular_params.get('degradation_rates')
+            model.reactions[i].forward_rates = 0
+    return model
+
+
+def compose_kwargs(internal_configs: dict = None, config_filepath: str = None, config_file: dict = None) -> dict:
     """ Extra configs like data paths can be supplied here, eg. for circuits that were dynamically generated. """
     def get_configs(config_file, config_filepath):
         config_filepath = make_filename_safely(config_filepath)
@@ -33,20 +46,19 @@ def compose_kwargs(extra_configs: dict = None, config_filepath: str = None, conf
         elif config_file is None and config_filepath is None:
             raise ValueError('Config file or path needed as input to function.')
         return config_file
+
     config_file = get_configs(config_file, config_filepath)
 
-    if extra_configs is not None:
-        for kwarg, config in extra_configs.items():
-            if kwarg != 'experiment': # and config_file.get(kwarg):
+    if internal_configs is not None:
+        for kwarg, config in internal_configs.items():
+            if kwarg != 'experiment':
                 config_file[kwarg] = config
-        # config_file.update(extra_configs)
+
     data_manager = DataManager(filepath=make_filename_safely(config_file.get("data_path", None)),
                                identities=config_file.get("identities", {}),
                                data=config_file.get("data", None),
                                sample_names=config_file.get("sample_names", None))
     molecular_params = load_json_as_dict(config_file.get("molecular_params"))
-    model_kwargs = compose_reaction_kwargs(data_manager)
-    bioreaction_model = construct_model(config)
 
     if type(config_file.get("molecular_params")) == dict:
         raise ValueError(
@@ -58,7 +70,7 @@ def compose_kwargs(extra_configs: dict = None, config_filepath: str = None, conf
         # For pre-loading interactions
         "interactions": config_file.get("interactions", {}),
         "interaction_simulator": config_file.get("interaction_simulator", {}),
-        "model": bioreaction_model,
+        "model": construct_bioreaction_model(data_manager.data.sample_names, molecular_params),
         "molecular_params": molecular_params,
         "mutations": cast_all_values_as_list(config_file.get("mutations", {})),
         "name": isolate_filename(data_manager.data.source),
@@ -79,41 +91,3 @@ def instantiate_system(kwargs):
     system_cfg_args = parse_cfg_args(kwargs)
     SystemType = get_system_type(kwargs["system_type"])
     return SystemType(system_cfg_args)
-
-
-def compose_reaction_kwargs(sample_names: list) -> dict:
-    kwargs = {}
-    kwargs['inputs'] = sample_names + pairup_combination(sample_names) + [[]] * len(sample_names)
-    kwargs['ouptuts'] = [[]] * len(sample_names) + pairup_combination(sample_names, astype=str) + sample_names
-
-    return kwargs
-
-
-def pairup_combination(samples, astype=list):
-    combination = []
-    for si in samples:
-        for sj in samples:
-            combination.append(tuple(sorted([si, sj])))
-    return sorted([astype(s) for s in (set(combination))])
-
-
-def construct_model(config: dict, sample_names):
-    from bioreaction.model.data_containers import BasicModel, Species, Reaction
-
-    model = BasicModel()
-
-    inputs = pairup_combination(sample_names)
-    outputs = pairup_combination(sample_names, astype=str)
-    ref_species = {s: Species(s) for s in set(
-        flatten_listlike(inputs + outputs, safe=True)) if s is not None}
-    for idx, (i, o) in enumerate(zip(inputs, outputs)):
-        reaction = Reaction()
-        reaction.input = make_species(i, ref_species)
-        reaction.output = combine_species(
-            i, ref_species) if o is None else make_species(o, ref_species)
-        reaction.forward_rate = forward_rates[idx]
-        reaction.reverse_rate = reverse_rates[idx]
-        model.reactions.append(reaction)
-
-    model.species = retrieve_species_from_reactions(model)
-    return model
