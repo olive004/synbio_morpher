bug: tabulation has some broadcast error

src/srv/sequence_exploration/sequence_analysis.py
@@ -166,7 +166,7 @@ def tabulate_mutation_info(source_dir, data_writer: DataWriter) -> pd.DataFrame:
             reference_v = reference_table[k]
             diff = np.where(np.asarray(reference_v) != 0, np.asarray(table[k]) - np.asarray(reference_v), 0)
             if np.shape(np.asarray(table[k])) < np.shape(np.asarray(reference_v)):
-                table[k] = np.repeat(table[k], repeats=len(reference_v))
+                table[k] = np.expand_dims(table[k], axis=1)
             ratio = np.divide(np.asarray(table[k]), np.asarray(reference_v),
                               out=np.zeros_like(np.asarray(table[k])), 
                               where=(np.asarray(reference_v) != 0 ) & (np.asarray(reference_v) != np.nan))

src/utils/circuit/agnostic_circuits/circuit_manager.py
@@ -334,11 +334,11 @@ class CircuitModeller():
                                                  circuit.species.species_axis])
             })]), axis=circuit.species.time_axis)
         if ref_circuit is None or ref_circuit == circuit:
-            ref_circuit_signal = None
+            ref_circuit_data = None
         else:
             ref_circuit_result = ref_circuit.result_collector.get_result(
                 'signal')
-            ref_circuit_signal = None if ref_circuit_result is None else ref_circuit_result.data
+            ref_circuit_data = None if ref_circuit_result is None else ref_circuit_result.data
 
         t = np.arange(0, np.shape(new_copynumbers)[
             1]) * modeller_signal.time_interval
@@ -351,7 +351,7 @@ class CircuitModeller():
                                                         'legend': list(circuit.species.data.sample_names),
                                                         'out_type': 'svg'},
                                             analytics_kwargs={'signal_idx': signal.identities_idx,
-                                                              'ref_circuit_signal': ref_circuit_signal})
+                                                              'ref_circuit_data': ref_circuit_data})
         return circuit
 
     def simulate_signal_batch(self, circuits: List[Tuple[str, BaseCircuit]], circuit_idx: int = 0,
@@ -399,11 +399,11 @@ class CircuitModeller():
 
         # Apply to all circuits
         if ref_circuit is None or ref_circuit == circuit:
-            ref_circuit_signal = None
+            ref_circuit_data = None
         else:
             ref_circuit_result = ref_circuit.result_collector.get_result(
                 'signal')
-            ref_circuit_signal = None if ref_circuit_result is None else ref_circuit_result.data
+            ref_circuit_data = None if ref_circuit_result is None else ref_circuit_result.data
         for i, circuit in enumerate(circuits):
             circuits[i].result_collector.add_result(b_new_copynumbers[i],
                                                     name='signal',
@@ -414,7 +414,7 @@ class CircuitModeller():
                                                                 'legend': list(circuit.species.data.sample_names),
                                                                 'out_type': 'svg'},
                                                     analytics_kwargs={'signal_idx': signal.identities_idx,
-                                                                      'ref_circuit_signal': ref_circuit_signal})
+                                                                      'ref_circuit_data': ref_circuit_data})
         return list(zip(names, circuits))
 
     # @time_it

src/utils/circuit/agnostic_circuits/circuit_manager_new.py
@@ -194,11 +194,11 @@ class CircuitModeller():
                 new_copynumbers = np.rollaxis(new_copynumbers, axis=1)
 
         if ref_circuit is None or ref_circuit == circuit:
-            ref_circuit_signal = None
+            ref_circuit_data = None
         else:
             ref_circuit_result = ref_circuit.result_collector.get_result(
                 'signal')
-            ref_circuit_signal = None if ref_circuit_result is None else ref_circuit_result.data
+            ref_circuit_data = None if ref_circuit_result is None else ref_circuit_result.data
 
         circuit.result_collector.add_result(
             data=new_copynumbers,
@@ -212,7 +212,7 @@ class CircuitModeller():
                         'out_type': 'svg'},
             analytics_kwargs={'labels': [s.name for s in circuit.model.species],
                               'signal_onehot': signal.onehot,
-                              'ref_circuit_signal': ref_circuit_signal})
+                              'ref_circuit_data': ref_circuit_data})
         return circuit
 
     def simulate_signal_batch(self, all_circuits: Dict[str, Dict[str, Circuit]],
@@ -264,13 +264,13 @@ class CircuitModeller():
 
         # Get analytics batched too
         if ref_circuit is None or ref_circuit == circuit:
-            ref_circuit_signal = None
+            ref_circuit_data = None
         else:
             ref_circuit_result = ref_circuit.result_collector.get_result(
                 'signal')
-            ref_circuit_signal = None if ref_circuit_result is None else ref_circuit_result.data.flatten()
+            ref_circuit_data = None if ref_circuit_result is None else ref_circuit_result.data.flatten()
         b_analytics = jax.vmap(partial(generate_analytics, time=t, labels=[s.name for s in circuits[circuit_idx].model.species],
-                                       signal_onehot=signal.onehot, ref_circuit_data=ref_circuit_signal))(data=b_new_copynumbers)
+                                       signal_onehot=signal.onehot, ref_circuit_data=ref_circuit_data))(data=b_new_copynumbers)
         b_analytics = [{k: v[i] for k, v in b_analytics.items()}
                        for i in range(len(circuits))]
 

src/utils/results/analytics/timeseries.py
@@ -30,7 +30,7 @@ def fold_change(data):
     #     return jnp.expand_dims(fold_change, axis=1)
     # else:
     #     return fold_change
-    return fold_change
+    return jnp.expand_dims(fold_change, axis=1)
 
 def get_overshoot(data, steady_states):
     return (jnp.expand_dims(jnp.max(data, axis=1), axis=1) - steady_states)
@@ -85,7 +85,7 @@ def get_rmse(data, ref_circuit_data):
     data = data - ref_circuit_data
     rmse = jnp.sqrt(
         jnp.sum(jnp.divide(jnp.power(data, 2), len(data)), axis=1))
-    return rmse
+    return jnp.expand_dims(rmse, axis=1)
 
 def get_step_response_times(data, t, steady_states, deriv1, signal_idx: jnp.ndarray):
     time = t * jnp.ones_like(steady_states)
@@ -120,6 +120,9 @@ def get_step_response_times(data, t, steady_states, deriv1, signal_idx: jnp.ndar
         jnp.max(time * argmax_workaround, axis=1), tstart)
 
     response_times = tstop - tstart
+
+    if response_times.ndim == 1:
+        return jnp.expand_dims(response_times, axis=1)
     return response_times
 
 def frequency(data):