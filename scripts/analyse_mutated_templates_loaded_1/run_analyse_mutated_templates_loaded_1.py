469482e7e872440ccc2ca16750008f2e29b68d65
olive004 <olivia.gallup@gmail.com>
2022-12-19 14:37:34 +0000

    visualise interactions

--- scripts/analyse_mutated_templates_loaded_1/run_analyse_mutated_templates_loaded_1.py
+++ scripts/analyse_mutated_templates_loaded_1/run_analyse_mutated_templates_loaded_1.py
@@ -15,6 +15,7 @@ from src.utils.results.analytics.timeseries import get_analytics_types, get_sign
 from src.utils.results.experiments import Experiment, Protocol
 from src.utils.results.result_writer import ResultWriter
 from src.utils.results.visualisation import visualise_data
+from src.srv.parameter_prediction.interactions import INTERACTION_TYPES
 from src.srv.parameter_prediction.simulator import SIMULATOR_UNITS
 from src.utils.data.data_format_tools.common import load_json_as_dict, load_json_mult
 
@@ -78,154 +79,12 @@ def main(config=None, data_writer=None):
             req_input=True,
             req_output=True,
             name='concatenate_dfs'
-        ),
-        # Binding rates min interactions mutations
-        Protocol(
-            partial(
-                visualise_data, data_writer=data_writer, cols_x=[
-                    'binding_rates_dissociation_min_interaction'],
-                plot_type='histplot',
-                hue='mutation_num',
-                out_name='binding_rates_dissociation_min_freqs_mutations_logs',
-                threshold_value_max=binding_rates_threshold_upper,
-                exclude_rows_zero_in_cols=['mutation_num'],
-                misc_histplot_kwargs={
-                    "hue": 'mutation_num',
-                    "multiple": "dodge",
-                    "element": "poly"},
-                log_axis=(True, False),
-                use_sns=True,
-                title=f'Minimum ' + r'$k_d$' + ' strength',
-                xlabel='Dissociation rate ' + r'$k_d$' + ' (' +
-                fr'{SIMULATOR_UNITS[source_config["interaction_simulator"]["name"]]["rate"]})' +
-                f'{binding_rates_threshold_upper_text}'),
-            req_input=True,
-            name='visualise_mutated_interactions'
-        ),
+        )
     ]
 
     # Visualisations
+    interaction_types = INTERACTION_TYPES
 
-    # Binding rates
-    rate_unit = r'{}'.format(
-        SIMULATOR_UNITS[source_config["interaction_simulator"]["name"]]["rate"])
-    protocols.append(Protocol(
-        partial(
-            visualise_data,
-            data_writer=data_writer,
-            cols_x=['binding_rates_dissociation_min_interaction'],
-            plot_type='histplot',
-            out_name='binding_rates_dissociation_min_freqs_mutations_logs',
-            threshold_value_max=binding_rates_threshold_upper,
-            exclude_rows_zero_in_cols=['mutation_num'],
-            misc_histplot_kwargs={
-                "hue": 'mutation_num',
-                "multiple": "dodge",
-                "element": "poly"},
-            log_axis=(True, False),
-            use_sns=True,
-            hue='mutation_num',
-            title=f'Minimum ' + r'$k_d$' + ' strength',
-            xlabel='Dissociation rate ' + r'$k_d$' + ' (' +
-            f'{rate_unit})' +
-            f'{binding_rates_threshold_upper_text}'),
-        req_input=True,
-        name='visualise_mutated_interactions'
-    ))
-    # Plot mean of interacting numbers
-    protocols.append(Protocol(
-        partial(
-            visualise_data, data_writer=data_writer,
-            cols_x=['mutation_num'],
-            cols_y=['num_interacting'],
-            plot_type='bar_plot',
-            out_name='num_interacting_barplot',
-            threshold_value_max=binding_rates_threshold_upper,
-            exclude_rows_zero_in_cols=['mutation_num'],
-            use_sns=True,
-            ci="sd",
-            title=f'Minimum ' + r'$k_d$' + ' strength',
-            ylabel='Number of interactions',
-            xlabel='Number of mutations'),
-        req_input=True,
-        name='visualise_mutated_interactions'
-    ))
-
-    # Analytics visualisation
-    analytics_types = get_analytics_types()
-    # Log histplots with mutation number hue
-    # Difference
-    for filltype in ['dodge', 'fill']:
-        for analytics_type, cols_x, title, xlabel in [
-                [
-                    analytics_type,
-                    f'{analytics_type}_diff_to_base_circuit',
-                    f'{prettify_keys_for_label(analytics_type)} difference between circuit\nand mutated counterparts',
-                    f'{prettify_keys_for_label(analytics_type)} difference'
-                ] for analytics_type in analytics_types]:
-
-            complete_colx_by_str = analytics_type + \
-                '_wrt' if analytics_type in get_signal_dependent_analytics() else None
-            protocols.append(Protocol(
-                partial(
-                    visualise_data,
-                    data_writer=data_writer, cols_x=[cols_x],
-                    plot_type='histplot',
-                    hue='mutation_num',
-                    out_name=f'{cols_x}_log_{filltype}',
-                    complete_colx_by_str=complete_colx_by_str,
-                    exclude_rows_zero_in_cols=['mutation_num'],
-                    misc_histplot_kwargs={
-                        "multiple": filltype,
-                        "hue": 'mutation_num',
-                        "element": "step"
-                    },
-                    log_axis=(True, False),
-                    use_sns=True,
-                    title=title,
-                    xlabel=xlabel
-                ),
-                req_input=True,
-                name='visualise_interactions_difference',
-                skip=config_file.get('only_visualise_circuits', False)
-            ))
-
-    # Log histplots with mutation number hue
-    # Ratios
-    for filltype in ['dodge', 'fill']:
-        for analytics_type, cols_x, title, xlabel in [
-                [
-                    analytics_type,
-                    f'{analytics_type}_ratio_from_mutation_to_base',
-                    f'{prettify_keys_for_label(analytics_type)} ratio from mutated\nto original circuit',
-                    f'{prettify_keys_for_label(analytics_type)} ratio'
-                ] for analytics_type in analytics_types]:
-
-            complete_colx_by_str = analytics_type + \
-                '_wrt' if analytics_type in get_signal_dependent_analytics() else None
-            protocols.append(Protocol(
-                partial(
-                    visualise_data,
-                    data_writer=data_writer, cols_x=[cols_x],
-                    plot_type='histplot',
-                    hue='mutation_num',
-                    out_name=f'{cols_x}_log_{filltype}',
-                    exclude_rows_zero_in_cols=['mutation_num'],
-                    complete_colx_by_str=complete_colx_by_str,
-                    misc_histplot_kwargs={
-                        "multiple": filltype,
-                        "hue": 'mutation_num',
-                        "element": "step"
-                    },
-                    log_axis=(True, False),
-                    use_sns=True,
-                    title=title,
-                    xlabel=xlabel
-                ),
-                req_input=True,
-                name='visualise_interactions_difference',
-                skip=config_file.get('only_visualise_circuits', False)
-            ))
 
     experiment = Experiment(config=config, config_file=config_file, protocols=protocols,
                             data_writer=data_writer)
