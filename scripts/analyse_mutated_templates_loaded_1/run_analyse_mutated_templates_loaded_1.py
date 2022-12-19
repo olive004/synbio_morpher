fe9d67c253bb019c9d2a1b795509d28245e9aa30
olive004 <olivia.gallup@gmail.com>
2022-12-19 16:54:45 +0000

    visualisation of all interactions

--- scripts/analyse_mutated_templates_loaded_1/run_analyse_mutated_templates_loaded_1.py
+++ scripts/analyse_mutated_templates_loaded_1/run_analyse_mutated_templates_loaded_1.py
@@ -1,6 +1,7 @@
 from functools import partial
 import logging
 import os
+import operator
 
 import numpy as np
 import pandas as pd
@@ -10,7 +11,7 @@ from src.srv.io.manage.script_manager import script_preamble
 from src.utils.misc.io import get_pathnames_from_mult_dirs
 from src.utils.misc.scripts_io import get_search_dir, load_experiment_config_original
 from src.utils.misc.string_handling import prettify_keys_for_label
-from src.utils.results.analytics.timeseries import get_analytics_types, get_signal_dependent_analytics
+from src.utils.results.analytics.timeseries import get_analytics_types, get_signal_dependent_analytics, DIFF_KEY, RATIO_KEY
 
 from src.utils.results.experiments import Experiment, Protocol
 from src.utils.results.result_writer import ResultWriter
@@ -39,9 +40,10 @@ def main(config=None, data_writer=None):
         config_searchdir_key='source_dirs', config_file=config_file)
     if type(source_dirs) != list:
         source_dirs = [source_dirs]
-    source_dir = source_dirs[0]
-    source_config = load_experiment_config_original(
-        source_dir, 'mutation_effect_on_interactions_signal')
+    # source_dir = source_dirs[0]
+    # source_config = load_experiment_config_original(
+    #     source_dir, 'mutation_effect_on_interactions_signal')
+    
 
     # binding_rates_threshold_upper = np.power(10, 6)
     binding_rates_threshold_upper = None
@@ -82,7 +84,50 @@ def main(config=None, data_writer=None):
     ]
 
     # Visualisations
-    interaction_types = INTERACTION_TYPES
+    def visualise_interactions_raw(data: pd.DataFrame, interaction_types, data_writer):
+        log_opts = [(False, False), (True, False)]
+        num_mutations = list(data['mutation_num'].unique())
+        for interaction_type in interaction_types:
+            interaction_cols = [c for c in data.columns if interaction_type in c and DIFF_KEY not in c and RATIO_KEY not in c and 'max' not in c and 'min' not in c]
+            for log_opt in log_opts:
+                log_text = '_log' if any(log_opt) else ''
+                for m in num_mutations+['all'] + ['all-pooled']:
+                    if 'all' in m:
+                        plot_grammar_m = 's'
+                        hue = 'mutation_num'
+                        selection_conditions = None
+                        if m == 'all-pooled':
+                            hue = None
+                    else:
+                        plot_grammar_m = 's' if m > 1 else ''
+                        hue = None
+                        selection_conditions = [('mutation_num', operator.eq, m)]
+
+                    visualise_data(
+                        og_data=data,
+                        cols_x=interaction_cols,
+                        plot_type='histplot',
+                        data_writer=data_writer,
+                        out_name=interaction_type + log_text + '_m' + m,
+                        hue=hue,
+                        selection_conditions=selection_conditions,
+                        log_axis=log_opt,
+                        use_sns=True,
+                        title=f'{prettify_keys_for_label(interaction_type)} for {m} mutation{plot_grammar_m}'
+                    )
+            
+
+    protocols.append(
+        Protocol(
+            partial(
+                visualise_interactions_raw,
+                interaction_types=INTERACTION_TYPES,
+                data_writer=data_writer
+            )
+            req_input=True,
+            name='visualise'
+        )
+    )
 
 
     experiment = Experiment(config=config, config_file=config_file, protocols=protocols,
