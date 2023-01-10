ed511b165dea8c220f306f06083c79c2c8abdb7b
olive004 <olivia.gallup@gmail.com>
2023-01-10 22:27:05 +0000

    ctr cannot build

--- setup/start_docker.sh
+++ setup/start_docker.sh
@@ -3,22 +3,22 @@ docker pull docker/dockerfile:1
 docker pull nvidia/cuda:11.6.2-base-ubuntu20.04
 
 # If image not built yet
-sudo ctr build -t genetic_glitch:latest docker_unix
+docker build -t genetic_glitch:latest docker_unix
 
 # sudo ctr run --rm -t \
 #     --runc-binary=/usr/bin/nvidia-container-runtime \
 #     --env NVIDIA_VISIBLE_DEVICES=all \
 #     docker.io/nvidia/cuda:11.6.2-base-ubuntu20.04 \
 #     cuda-11.6.2-base-ubuntu20.04 nvidia-smi
-sudo ctr create -it \
+docker create -it \
 --rm \
 --runc-binary=/usr/bin/nvidia-container-runtime \
 --env NVIDIA_VISIBLE_DEVICES=all \
 --name gcg_test \
 --mount type=bind,source="$(pwd)",target=/workdir \
 genetic_glitch:latest
-sudo ctr container start gcg_test
-sudo ctr exec -it gcg_test /bin/bash 
+docker container start gcg_test
+docker exec -it gcg_test /bin/bash 
 # docker container stop gcg
 
 
