082f50ae866cacd64748cfda06393275df85ce74
olive004 <olivia.gallup@gmail.com>
2023-06-14 09:52:28 +0100

    sudo on docker setup

--- setup/start_docker.sh
+++ setup/start_docker.sh
@@ -1,30 +1,30 @@
 
 # A pre-req for using gpus here is the NVIDIA Docker Container Toolkit
 
-docker pull docker/dockerfile:1
-docker pull quay.io/biocontainers/intarna:3.3.2--pl5321h7ff8a90_0
-# docker pull nvidia/cuda:12.1.0-devel-ubuntu22.04
-docker pull nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04
+sudo docker pull docker/dockerfile:1
+sudo docker pull quay.io/biocontainers/intarna:3.3.2--pl5321h7ff8a90_0
+# sudo docker pull nvidia/cuda:12.1.0-devel-ubuntu22.04
+sudo docker pull nvidia/cuda:11.8.0-cudnn8-devel-ubuntu22.04
 
 # If image not built yet
-docker build -t genetic_glitch:latest docker_unix
+sudo docker build -t genetic_glitch:latest docker_unix
 
 # sudo ctr run --rm -t \
 #     --runc-binary=/usr/bin/nvidia-container-runtime \
 #     --env NVIDIA_VISIBLE_DEVICES=all \
-#     docker.io/nvidia/cuda:11.6.2-base-ubuntu20.04 \
+#     sudo docker.io/nvidia/cuda:11.6.2-base-ubuntu20.04 \
 #     cuda-11.6.2-base-ubuntu20.04 nvidia-smi
 cp ./requirements.txt ./docker_unix
 
-docker create -it \
+sudo docker create -it \
 --rm \
 --gpus all \
 --name gcg \
 --mount type=bind,source="$(pwd)",target=/workdir \
 genetic_glitch:latest
-docker container start gcg
-docker exec -it gcg /bin/bash 
-# docker container stop gcg
+sudo docker container start gcg
+sudo docker exec -it gcg /bin/bash 
+# sudo docker container stop gcg
 
 
 # Docusaurus
