954ede5c8bb1eb2054172c61f3a9192b14f19374
olive004 <olivia.gallup@gmail.com>
2023-07-13 11:20:30 +0100

    removing og states

--- synbio_morpher/srv/io/manage/script_manager.py
+++ synbio_morpher/srv/io/manage/script_manager.py
@@ -37,7 +37,7 @@ def import_script_func(script_name):
     return getattr(script_module, 'main')
 
 
-def script_preamble(config, data_writer, alt_cfg_filepath: str = None, use_resultwriter=True) -> Tuple[dict, DataWriter]:
+def script_preamble(config, data_writer = None, alt_cfg_filepath: str = None, use_resultwriter=True) -> Tuple[dict, DataWriter]:
     Writer = ResultWriter if use_resultwriter else DataWriter
     if config is None:
         config = alt_cfg_filepath
