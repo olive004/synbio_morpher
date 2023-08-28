fixed precision

synbio_morpher/scripts/parameter_grid_analysis/run_parameter_grid_analysis.py
@@ -34,9 +34,8 @@ def main(config=None, data_writer: ResultWriter = None):
         'synbio_morpher', 'scripts', 'parameter_grid_analysis', 'configs', 'base_config.json'))
 
     # Load in parameter grid
-    config_file = load_json_as_dict(config)
-    config_file, source_dir = get_search_dir(
-        config_searchdir_key='source_parameter_dir', config_file=config_file)
+    config, source_dir = get_search_dir(
+        config_searchdir_key='source_parameter_dir', config_file=config)
 
     def load_parameter_grids():
         all_parameter_grids = {}
@@ -66,10 +65,7 @@ def main(config=None, data_writer: ResultWriter = None):
         f'not equal number of species in original configuration file {sample_names}.'
 
     # Get relevant configs
-    slicing_configs = config_file['slicing']
-
-    selected_species_interactions = slicing_configs['interactions']['interacting_species']
-    unselected_species_interactions = slicing_configs['interactions']['non_varying_species_interactions']
+    slicing_configs = config['slicing']
 
     selected_analytics = slicing_configs['analytics']['names']
     if selected_analytics is None:
@@ -83,14 +79,14 @@ def main(config=None, data_writer: ResultWriter = None):
                 f'Species {cfg_species_list} from config were not found in list ' \
                 f'of species from circuit {sample_names}'
 
-    validate_species_cfgs(flatten_listlike(list(selected_species_interactions.values())),
-                          flatten_listlike(list(unselected_species_interactions.values())))
+    validate_species_cfgs(flatten_listlike(list(slicing_configs['interactions']['interacting_species'].values())),
+                          flatten_listlike(list(slicing_configs['interactions']['non_varying_species_interactions'].values())))
 
     def run_visualisation(all_parameter_grids, data_writer, selected_analytics,
                           selected_species_interactions, unselected_species_interactions,
                           slicing_configs, sample_names, shape_parameter_grid):
 
-        def visualise_analytic(analytic_name, data):
+        def visualise_analytic(analytic_name: str, data: np.ndarray, selected_species_interactions: list):
             result_collector = ResultCollector()
             for i, species_name in enumerate(slicing_configs['species_choices']):
                 data_per_species = data[i]
@@ -130,7 +126,8 @@ def main(config=None, data_writer: ResultWriter = None):
                                       no_analytics=True, no_numerical=True)
 
         species_interaction_summary = make_species_interaction_summary(
-            selected_species_interactions, slicing_configs['interactions']['strengths'],
+            species_interactions=selected_species_interactions, 
+            strength_config=slicing_configs['interactions']['strengths'],
             original_config=original_config, sample_names=sample_names)
 
         slice_indices = make_slice(selected_species_interactions, unselected_species_interactions, slicing_configs,
@@ -141,15 +138,15 @@ def main(config=None, data_writer: ResultWriter = None):
         )
         for analytic_name in selected_analytics:
             data = all_parameter_grids[analytic_name][slice_indices]
-            visualise_analytic(analytic_name, data)
+            visualise_analytic(analytic_name, data, selected_species_interactions)
 
-    experiment = Experiment(config=config, config_file=config_file, protocols=[
+    experiment = Experiment(config=config, config_file=config, protocols=[
         Protocol(partial(run_visualisation,
                          all_parameter_grids=all_parameter_grids,
                          data_writer=data_writer,
                          selected_analytics=selected_analytics,
-                         selected_species_interactions=selected_species_interactions,
-                         unselected_species_interactions=unselected_species_interactions,
+                         selected_species_interactions=slicing_configs['interactions']['interacting_species'],
+                         unselected_species_interactions=slicing_configs['interactions']['non_varying_species_interactions'],
                          slicing_configs=slicing_configs,
                          sample_names=sample_names,
                          shape_parameter_grid=shape_parameter_grid))

synbio_morpher/utils/parameter_inference/interpolation_grid.py
@@ -47,21 +47,21 @@ def create_parameter_range(range_configs: dict) -> np.ndarray:
 
 
 # Helper funcs
-def convert_parameter_values_to_slice(start_value, end_value, step_size, original_config) -> Union[slice, tuple]:
+def convert_parameter_values_to_slice(interaction_min, interaction_max, step_size, original_config) -> Union[slice, tuple]:
 
     def parse_parameter_range_kwargs(start_value, end_value, original_parameter_range):
         if start_value is None:
-            start_value = np.float64(0)
+            start_value = original_parameter_range[0]
         if end_value is None:
-            end_value = np.float64(original_parameter_range[-1])
-        return np.float64(start_value), np.float64(end_value)
+            end_value = original_parameter_range[-1]
+        return start_value, end_value
 
     original_parameter_range = create_parameter_range(
         original_config['parameter_based_simulation'])
-    start_value, end_value = parse_parameter_range_kwargs(start_value, end_value,
+    interaction_min, interaction_max = parse_parameter_range_kwargs(interaction_min, interaction_max,
                                                             original_parameter_range)
     selected_parameter_range_idxs = np.arange(len(
-        original_parameter_range))[(original_parameter_range >= start_value) == (original_parameter_range <= end_value)]
+        original_parameter_range))[(original_parameter_range >= interaction_min) == (original_parameter_range <= interaction_max)]
     if len(selected_parameter_range_idxs) > 1:
         return slice(
             selected_parameter_range_idxs[0], selected_parameter_range_idxs[-1]+1) #, step_size)
@@ -72,7 +72,7 @@ def convert_parameter_values_to_slice(start_value, end_value, step_size, origina
 
 
 # Convenience table
-def make_species_interaction_summary(species_interactions, strength_config, original_config, sample_names):
+def make_species_interaction_summary(species_interactions, strength_config, original_config, sample_names) -> dict:
     all_refs = {}
     for grouping, species_interaction in species_interactions.items():
         species_interactions_ref = {}
@@ -87,15 +87,16 @@ def make_species_interaction_summary(species_interactions, strength_config, orig
         for k, v in parameter_creation_cfg.items():
             if v is None:
                 parameter_creation_cfg[k] = original_config['parameter_based_simulation'][k]
+        
         species_interactions_ref['parameter_range'] = create_parameter_range(
-            parameter_creation_cfg)
+            original_config['parameter_based_simulation'])
 
         if type(strength_config[grouping]) == dict:
             species_interactions_ref['interaction_slice'] = convert_parameter_values_to_slice(
                 original_config=original_config, **strength_config[grouping])
         else:
             species_interactions_ref['interaction_slice'] = convert_parameter_values_to_slice(
-                start_value=strength_config[grouping], end_value=strength_config[grouping],
+                interaction_min=strength_config[grouping], interaction_max=strength_config[grouping],
                 step_size=None, original_config=original_config)
         all_refs[tuple(species_interaction)] = species_interactions_ref
     return all_refs
@@ -164,10 +165,8 @@ def make_slice(selected_species_interactions, unselected_species_interactions,
                     original_config=original_config, **parameter_config[grouping])
             else:
                 slice_idx_map[interaction_idx] = convert_parameter_values_to_slice(
-                    start_value=parameter_config[grouping], end_value=parameter_config[grouping],
+                    interaction_min=parameter_config[grouping], interaction_max=parameter_config[grouping],
                     step_size=None, original_config=original_config)
-
-        logging.info(parameter_config)
 
         return slice_idx_map
 

synbio_morpher/utils/results/analytics/timeseries.py
@@ -221,7 +221,7 @@ def generate_base_analytics(data: jnp.ndarray, t: jnp.ndarray, labels: List[str]
                 starting_states=analytics['initial_steady_states'],
                 steady_states=analytics['steady_states'],
                 signal_0=analytics['initial_steady_states'][s_idx],
-                signal_1=data[s_idx, np.argmax(t == signal_time)]
+                signal_1=data[s_idx, np.argmax(t >= signal_time)]
             )
 
             # t axis: 1