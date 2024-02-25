0c221d4f4edc64d8020a3123c373c90fc811c8ab
olive004 <olivia.gallup@gmail.com>
2024-02-25 18:05:55 +0000

    sensitivity mode

--- synbio_morpher/utils/modelling/physical.py
+++ synbio_morpher/utils/modelling/physical.py
@@ -9,19 +9,21 @@ import numpy as np
 from synbio_morpher.utils.misc.units import SCIENTIFIC
 
 
-def F(E):
-    """ Parameterisation of relative GFP fluorescence binding from 
-    paper [Metabolic engineering of Escherichia coli using synthetic small regulatory RNAs](
-    https://www.nature.com/articles/nbt.2461
-    ). See the notebook `explanations/binding_energy_reparameterisation` for 
-    more details.
-    The binding energy is in units of kcal/mol """
-    F = (1-0.01)/(1+np.exp(-(E/2 + 5))) + 0.01
-    return F
+# def F(E):
+#     """ Parameterisation of relative GFP fluorescence binding from 
+#     paper [Metabolic engineering of Escherichia coli using synthetic small regulatory RNAs](
+#     https://www.nature.com/articles/nbt.2461
+#     ). See the notebook `explanations/binding_energy_reparameterisation` for 
+#     more details.
+#     The binding energy is in units of kcal/mol """
+#     F = (1-0.01)/(1+np.exp(-(E/2 + 5))) + 0.01
+#     return F
 
 
 def equilibrium_constant_reparameterisation(E, initial: np.array):
-    """ The energy input E is $\Delta G$ in kcal/mol. 
+    """ Input: E is $\Delta G$ of binding in kcal/mol. 
+    Output: equilibrium constant
+
     IMPORTANT: Using the mean initial quantity of all species, as this 
     equation was derived under the assumption that all unbound species 
     start with the same concentration and have the same interactions """
