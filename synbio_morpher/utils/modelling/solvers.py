54c453b295c6fa772192e50be422adc2e92b533e
olive004 <olivia.gallup@gmail.com>
2024-03-22 00:20:37 +0000

    init solvers

--- synbio_morpher/utils/modelling/solvers.py
+++ synbio_morpher/utils/modelling/solvers.py
@@ -6,25 +6,25 @@ from synbio_morpher.srv.parameter_prediction.simulator import make_piecewise_ste
 def get_diffrax_solver(solver_type):
 
     solvers = {
-        'Tsit5': dfx.Tsit5,
-        'Dopri5': dfx.Dopri5,
-        'Dopri8': dfx.Dopri8,
-        'Heun': dfx.Heun,
-        'Euler': dfx.Euler,
-        'Midpoint': dfx.Midpoint,
-        'Ralston': dfx.Ralston,
-        'Bosh3': dfx.Bosh3,
-        'ImplicitEuler': dfx.ImplicitEuler,
-        'Kvaerno3': dfx.Kvaerno3,
-        'Kvaerno4': dfx.Kvaerno4,
-        'Kvaerno5': dfx.Kvaerno5,
-        'Sil3': dfx.Sil3,
-        'KenCarp3': dfx.KenCarp3,
-        'KenCarp4': dfx.KenCarp4,
-        'KenCarp5': dfx.KenCarp5,
-        'SemiImplicitEuler': dfx.SemiImplicitEuler,
-        'ReversibleHeun': dfx.ReversibleHeun,
-        'LeapfrogMidpoint': dfx.LeapfrogMidpoint
+        'Tsit5': dfx.Tsit5(),
+        'Dopri5': dfx.Dopri5(),
+        'Dopri8': dfx.Dopri8(),
+        'Heun': dfx.Heun(),
+        'Euler': dfx.Euler(),
+        'Midpoint': dfx.Midpoint(),
+        'Ralston': dfx.Ralston(),
+        'Bosh3': dfx.Bosh3(),
+        'ImplicitEuler': dfx.ImplicitEuler(),
+        'Kvaerno3': dfx.Kvaerno3(),
+        'Kvaerno4': dfx.Kvaerno4(),
+        'Kvaerno5': dfx.Kvaerno5(),
+        'Sil3': dfx.Sil3(),
+        'KenCarp3': dfx.KenCarp3(),
+        'KenCarp4': dfx.KenCarp4(),
+        'KenCarp5': dfx.KenCarp5(),
+        'SemiImplicitEuler': dfx.SemiImplicitEuler(),
+        'ReversibleHeun': dfx.ReversibleHeun(),
+        'LeapfrogMidpoint': dfx.LeapfrogMidpoint()
     }
 
     assert solver_type in list(solvers.keys(
