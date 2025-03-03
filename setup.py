e0381053fee853179fecb4766ef0496dffdd9b5d
olive004 <olivia.gallup@gmail.com>
2025-03-03 19:02:56 +0000

    vers up

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
         authors=[
             {'name': "Olivia Gallup", 'email': "olivia.gallup@gmail.com"}
         ],
-        version='0.0.901',
+        version='0.0.902',
         license='LICENSE',
         description='Build, simulate, and analyse genetic circuits and the effect of mutations.',
         readme="README.md",
